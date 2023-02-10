3f298b7571a4893c466554ee644137b88840cea0
HermanoNeto <hermano_neto.b@hotmail.com>
2023-02-10 01:34:57 -0300

    Adding Calculator

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double resultado = 0;
+            Console.WriteLine("Escolha uma operação: \n(Soma)\n(Divisão)\n(Multiplicação)\n(Subtração)");
+            string operacao = Console.ReadLine().ToLower();
+
+            Console.WriteLine("Digite dois números:");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+
+            if (operacao == "soma")
+            {
+                resultado = Soma(n1, n2);
+            }
+            else if (operacao == "divisao")
+            {
+                resultado = Divisao(n1, n2);
+            }
+            else if (operacao == "multiplicacao")
+            {
+                resultado = Multiplicacao(n1, n2);
+            }
+            else if (operacao == "subtracao")
+            {
+                resultado = Subtracao(n1, n2);
+            }
+            Console.WriteLine("A {0} dos dois números é: {1}", operacao, resultado);
+
+
+        }
+
+        static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double Divisao(double a, double b)
+        {
+            return a / b;
+        }
+
+        static int Multiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
